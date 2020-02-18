41d7a7907a3470504424c887ea48d7b8fcc13c49
Gary McAdam <gary@mcad.am>
2020-02-18 15:42:07 +0000

    formatting

--- fetch-with-certs.js
+++ fetch-with-certs.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const request = require("request-promise");
+const request = require('request-promise');
 
-const CA_PATH = process.env.CA_PATH || "/etc/pki/tls/certs/ca-bundle.crt";
-const CERT_PATH = process.env.CERT_PATH || "/etc/pki/tls/certs/client.crt";
-const KEY_PATH = process.env.KEY_PATH || "/etc/pki/tls/private/client.key";
+const CA_PATH = process.env.CA_PATH || '/etc/pki/tls/certs/ca-bundle.crt';
+const CERT_PATH = process.env.CERT_PATH || '/etc/pki/tls/certs/client.crt';
+const KEY_PATH = process.env.KEY_PATH || '/etc/pki/tls/private/client.key';
 
 const defaultOptions = {
-	ca: fs.readFileSync(CA_PATH),
-	cert: fs.readFileSync(CERT_PATH),
-	key: fs.readFileSync(KEY_PATH),
-	keepAlive: true,
-	rejectUnauthorized: false,
-	secureProtocol: "TLSv1_method"
+  ca: fs.readFileSync(CA_PATH),
+  cert: fs.readFileSync(CERT_PATH),
+  key: fs.readFileSync(KEY_PATH),
+  keepAlive: true,
+  rejectUnauthorized: false,
+  secureProtocol: 'TLSv1_method'
 };
 
 module.exports = options => {
-	return request({ ...defaultOptions, ...options });
+  return request({ ...defaultOptions, ...options });
 };
