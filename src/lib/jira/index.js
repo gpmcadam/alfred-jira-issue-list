ab47d9ce7d6a1d0e71b3623bda64341943f1dd6e
Gary McAdam <gary@mcad.am>
2020-02-19 00:26:30 +0000

    Pull out issue data after fetching from JIRA

--- src/lib/jira/index.js
+++ src/lib/jira/index.js
@@ -3,7 +3,7 @@ const request = require('request-promise');
 const getIssues = ({ jiraUrl, project, rapidViewId, options }) => {
   const URL = `${jiraUrl}/rest/greenhopper/1.0/xboard/work/allData.json?rapidViewId=${rapidViewId}&selectedProjectKey=${project}`;
 
-  return request({ ...options, uri: URL, json: true });
+  return request({ ...options, uri: URL, json: true }).then(res => res.issuesData.issues);
 };
 
 module.exports = { getIssues };
