b3d9b81639fa09690e152679987f3e57aabd62b6
Gary McAdam <gary@mcad.am>
2022-07-05 12:37:54 +0100

    add coverage for required project envs

--- src/workflow/workflow.test.js
+++ src/workflow/workflow.test.js
@@ -19,18 +19,30 @@ afterEach(() => {
   jest.resetAllMocks();
 });
 
-process.env.PROJECT = 'proj';
-process.env.RAPIDVIEWID = 'rvi';
-process.env.JIRA_URL = 'ju';
-process.env.API_USER = 'apiu';
-process.env.API_KEY = 'apik';
-
-test('alfy app outputs a valid list', async () => {
-  const alfy = await import('alfy');
+beforeEach(() => {
   jest.unstable_mockModule('../alfred-jira', () => ({
     default: () => Promise.resolve(mockResults),
   }));
-  const { default: workflow } = await import('.');
+  process.env.PROJECT = 'proj';
+  process.env.RAPIDVIEWID = 'rvi';
+  process.env.JIRA_URL = 'ju';
+  process.env.API_USER = 'apiu';
+  process.env.API_KEY = 'apik';
+});
+
+test.each(['PROJECT', 'RAPIDVIEWID', 'JIRA_URL'])('fails on missing %s', async (key) => {
+  delete process.env[key];
+  try {
+    const { default: workflow } = await import('./index.js');
+    await workflow();
+  } catch (e) {
+    expect(e.message).toContain(`Please configure the plugin. Missing: ${key}`);
+  }
+});
+
+test('alfy app outputs a valid list', async () => {
+  const alfy = await import('alfy');
+  const { default: workflow } = await import('./index.js');
   await workflow();
   expect(alfy.default.output).toHaveBeenCalledWith(mockResults);
 });
@@ -38,7 +50,7 @@ test('alfy app outputs a valid list', async () => {
 test('alfy app passes appropriate config to jira', async () => {
   jest.unstable_mockModule('../alfred-jira', () => ({ default: jest.fn() }));
   const alfredJira = await import('../alfred-jira');
-  const { default: workflow } = await import('.');
+  const { default: workflow } = await import('./index.js');
 
   await workflow();
   expect(alfredJira.default).toHaveBeenCalledWith({
@@ -53,7 +65,7 @@ test('alfy app passes appropriate config to jira', async () => {
 test('alfy app reports errors using alfy.error', async () => {
   const alfy = await import('alfy');
   jest.unstable_mockModule('../alfred-jira', () => ({ default: () => Promise.reject(new Error('foobarbaz')) }));
-  const { default: workflow } = await import('.');
+  const { default: workflow } = await import('./index.js');
   await workflow();
 
   expect(alfy.default.output).not.toHaveBeenCalled();
